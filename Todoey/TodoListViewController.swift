20a62f060b71fd41f54218b89c47c2a685b42951
Amir Bakhshi <amirmobili7@gmail.com>
2021-09-09 15:38:48 +0200

    implement uialert with different actions

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,14 +9,16 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
     var itemArray = ["Tyska1", "Dari1", "PRO1"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-//MARK: - TableViewDataSource Methods
+    
+
+    //MARK: - TableViewDataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -24,17 +26,17 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
-            
+        
         return cell
     }
     
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-  
-//        print(itemArray[indexPath.row])
+        
+        //        print(itemArray[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
- 
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -42,39 +44,40 @@ class TodoListViewController: UITableViewController {
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-    }
-    
-    //MARK: - Add New Items
-    
-    @IBAction func addPressed(_ sender: UIBarButtonItem) {
-        
-        // init an empty UITextField
-        var alertTextFieldInput = UITextField()
-        
-        let alert = UIAlertController(title: "Add New Course", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            // what will happen once user clicks the add btn
-            
-            self.itemArray.append(alertTextFieldInput.text!)
-            self.tableView.reloadData()
-            
-        }
-        
-        
-        
-        alert.addTextField { alertTextField in
-            alertTextField.placeholder = "e.g. Math 2"
-            alertTextFieldInput = alertTextField
-        }
-        // 1. create alert
-        // 2. create action
-        // 3. add the action to alert
-        // 4. present the alert (includes the action)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        
         
     }
+    
+//    MARK: - Add New Items
+    
+        @IBAction func addPressed(_ sender: UIBarButtonItem) {
+    
+            // init an empty UITextField
+            var alertTextFieldInput = UITextField()
+    
+            let alert = UIAlertController(title: "Add New Course", message: "", preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let action = UIAlertAction(title: "Add Item", style: .default) { action in
+                // what will happen once user clicks the add btn
+    
+                self.itemArray.append(alertTextFieldInput.text!)
+                self.tableView.reloadData()
+    
+            }
+    
+            alert.addTextField { alertTextField in
+                alertTextField.placeholder = "e.g. Math 2"
+                alertTextFieldInput = alertTextField
+            }
+            // 1. create alert
+            // 2. create action
+            // 3. add the action to alert
+            // 4. present the alert (includes the action)
+            alert.addAction(action)
+            alert.addAction(cancel)
+            present(alert, animated: true, completion: nil)
+        }
+    
+
     
 }
 
